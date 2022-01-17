58911a052f1938af33e1c650f4a821c5861581f5
Alex Oliveira <kurko@users.noreply.github.com>
2022-01-16 22:12:52 -0300

    Initial files

---
+++ lib/portoseguro/ynab/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Portoseguro
+  module Ynab
+    VERSION = "0.1.0"
+  end
+end
---
+++ spec/portoseguro/ynab_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Portoseguro::Ynab do
+  it "has a version number" do
+    expect(Portoseguro::Ynab::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
